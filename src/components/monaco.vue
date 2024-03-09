95b028b91ec42703a4c20bfdfb3804bdebc07d8e
gantrol.q <501963347@qq.com>
2024-03-09 13:40:41 +0800

    modified: add spin

--- src/components/monaco.vue
+++ src/components/monaco.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="editor">
+    <div v-if="isLoading" class="loader"></div>
     <div
         :ref="props.id"
         :id="props.id"
@@ -13,10 +14,26 @@
   margin: 2em;
   overflow: hidden;
 }
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.loader {
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+  margin: 20px auto;
+}
+
 </style>
 
 <script setup>
-import {onMounted} from "vue";
+import {nextTick, onMounted, ref} from "vue";
 /**
  * https://github.com/vuejs/vitepress/issues/1508
  * https://github.com/vitejs/vite/discussions/1791#discussioncomment-321046
@@ -38,6 +55,7 @@ const props = defineProps({
 
 const emit = defineEmits(['update:initialValue']);
 
+const isLoading = ref(true); // 设置为 true，因为初始状态是正在加载
 
 let editorManager;
 
@@ -59,7 +77,8 @@ self.MonacoEnvironment = {
   },
 };
 
-onMounted(() => {
+onMounted(async () => {
+  // await new Promise((resolve) => {setTimeout(resolve, 3000)});  // for testing spin
   editorManager = monaco.editor.create(
       // TODO: id需要……
       document.getElementById(props.id),
@@ -75,5 +94,8 @@ onMounted(() => {
     // 发出事件，通知父组件更新 initialValue
     emit('update:initialValue', currentValue);
   });
+
+  await nextTick(); // 等待下一个 DOM 更新周期
+  isLoading.value = false; // 然后更新 isLoading 状态
 });
 </script>
